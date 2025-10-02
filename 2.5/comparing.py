7d586fd3299126d943734186b8f10cf2461f8806
StevenZhang3125 <2042427@learn.vsb.bc.ca>
2025-10-02 21:59:22 +0000

    Added code

--- 2.5/comparing.py
+++ 2.5/comparing.py
@@ -9,17 +9,29 @@ Test as you go! Describe in your comments what steps you took to test your code.
 
 """
 Idea:
-Similarity Leaderboard
-Requests the user to input a name and outputs a leaderboard of similarity values
+Similarity Leaderboard and Finder
+Requests the user to input a name and choose whether to output a leaderboard of similarity 
+values or whom is most/least similar to that person
 
-Algorithm:
+Algorithm (Most/Least Similar Person):
 1) Declare variables, convert file to List (More easily indexable)
-2) Skip the first line in the file and iterate through the rest of the data.
-3) Iterates through the file to find the inputed user and saves it a variable.
-4) Iterate through each element of each line to compare elements that could be compared (Favourite ____ and not Name or ID)
-5) Compare which element are equal and count to a temporary variable
-6) Add compared data to leaderboard
-7) Display who is the least similar to the inputed person and by how few
+2) Skip the first line in the file and iterate through the rest of the data
+3) Request a name and options
+4) Iterates through the file to find the inputed user and saves it a variable
+5) Iterate through each element of each line to compare elements that could be compared (Favourite ____ and not Name or ID)
+6) Compare which element are equal and count to a temporary variable
+7) Compare the temporary variable to the saved variable of the inputed person
+8) Display who is the most/least similar to the inputed person and by how much
+
+Algorithm (Leaderboard):
+1) Declare variables, convert file to List (More easily indexable)
+2) Skip the first line in the file and iterate through the rest of the data
+3) Request a name and options
+4) Iterates through the file to find the inputed user and saves it a variable
+5) Iterate through each element of each line to compare elements that could be compared (Favourite ____ and not Name or ID)
+6) Compare which element are equal and count to a temporary variable
+7) Add compared data to leaderboard
+8) Display who is the least similar to the inputed person and by how few
 
 Test Cases:
 Steven Zhang
@@ -28,7 +40,10 @@ Steven Zhang
 Steven Zhang
 2
 
-Steven zhAng!!?  
+Steven Zhang 
+3
+
+SteVen ZhAng!!?
 1
 
 Even Chen
@@ -40,39 +55,13 @@ file = list(open("2.4/responses.csv"))
 # Removes first line/element (Legend)
 junk = file.pop(0)
 
-while True:
-    # User Instructions
-    print("Similarity Leaderboard")
-    print("Enter an person and find out who is most to least (or vise versa) similar to that person!")
 
-    # User Input Requests
-    person = input("Name: ").lower().strip(".!? ")
-    order = int(input("1) Most to Least\n2) Least to Most\nEnter which one (1/2): "))
-
-    # Converts integer input (order) into boolean
-    if order == 1:
-        order = True
+def mostLeastFinder(personName, personData, most):
+    if most:
+        vertexSimilarity = 0
     else:
-        order = False
-
-    # Delcares empty leaderboard variable
-    leaderboard = []
-
-    # Find the inputed person and formats list (.split(","); Completing conversion from csv file to List)
-    # Declare empty list for inputed person; useful to determine if the person has been found
-    personLine = []
-    # Iterate through each line of the list and properly convert data to nested lists 
-    for i in range(len(file)):
-        file[i] = file[i].split(",")
-        # Checks if line belongs to entered person
-        if file[i][1].lower().strip(" .?!") == person:
-            # Sets empty list to entered person's data; shows person has been found
-            personLine = file[i]
-
-    # Checks person's list for changes, if no changes were made, the person was not found
-    if personLine == []:
-        print("\nSorry, could not find that person. Please check your spelling and try again.")
-        continue
+        vertexSimilarity = 999
+    similarityPerson = ""
 
     # Iterates through the list of people's data
     for i in range(len(file)):
@@ -84,7 +73,43 @@ while True:
             if k == 0 or k == 1:
                 continue
             # Skips self (entered user)
-            if file[i][1].lower().strip(" .?!") == person:
+            if file[i][1].lower().strip(" .?!") == person.lower():
+                continue
+            # If elements are the same, increase similarities by 1
+            if file[i][k].lower().strip(" .!?") == personLine[k].lower().strip(" .?!"):
+                similarities += 1
+        if most:
+            if similarities > vertexSimilarity:
+                vertexSimilarity = similarities
+                similarityPerson = file[i][1]
+        else:
+            if similarities < vertexSimilarity:
+                vertexSimilarity = similarities
+                similarityPerson = file[i][1]
+
+    # Formats display text according to sorted order
+    if most:
+        print("Most similiar")
+    else:
+        print("Least similiar")
+    
+    print("Person: " + similarityPerson + " | Similarity Count: " + str(vertexSimilarity))
+
+def leaderboardFinder(personName, personData):
+    # Delcares empty leaderboard variable
+    leaderboard = []
+
+    # Iterates through the list of people's data
+    for i in range(len(file)):
+        # Declares a variable, set to 0 for each person
+        similarities = 0
+        # Iterates through each element of the person 
+        for k in range(len(file[i])):
+            # Skips ID and Name elements
+            if k == 0 or k == 1:
+                continue
+            # Skips self (entered user)
+            if file[i][1].lower().strip(" .?!") == person.lower():
                 continue
             # If elements are the same, increase similarities by 1
             if file[i][k].lower().strip(" .!?") == personLine[k].lower().strip(" .?!"):
@@ -109,6 +134,48 @@ while True:
     # Iterates through the leaderboard and displays every element in request order
     for i in range(len(leaderboard)):
         print(str(i+1) + ") " + leaderboard[i][0] + " " + str(leaderboard[i][1]))
-    
-    # Exits out of while loop
+
+formatted = False
+while True:
+    # User Instructions
+    print("Similarity Leaderboard and Finder")
+    print("Enter an person and find out who are the most or least similar to that person!")
+
+    # User Input Requests
+    person = input("Name: ").strip(".!? ")
+    option = int(input("1) Most Similar\n2) Least Similar\n3) Leaderboard\nPlease enter a number (1/2/3): "))
+    if option == 3:
+        order = int(input("1) Most to Least\n2) Least to Most\nEnter which one (1/2): "))
+
+    # Find the inputed person and formats list (.split(","); Completing conversion from csv file to List)
+    # Declare empty list for inputed person; useful to determine if the person has been found
+    personLine = []
+
+    # Iterate through each line of the list and properly convert data to nested lists 
+    for i in range(len(file)):
+        if not formatted:
+            file[i] = file[i].split(",")
+        # Checks if line belongs to entered person
+        if file[i][1].lower().strip(" .?!") == person.lower():
+            # Sets empty list to entered person's data; shows person has been found
+            personLine = file[i]
+    formatted = True
+
+    # Checks person's list for changes, if no changes were made, the person was not found
+    if personLine == []:
+        print("\nSorry, could not find that person. Please check your spelling and try again.")
+        continue
+
+    if option == 1:
+        mostLeastFinder(person, personLine, True)
+    elif option == 2:
+        mostLeastFinder(person, personLine, False)
+    else:
+        if order == 1:
+            order = True
+        else:
+            order = False
+        leaderboardFinder(person, personLine)
+        
     break
+
