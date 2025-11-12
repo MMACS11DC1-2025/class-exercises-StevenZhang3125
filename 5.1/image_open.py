50989362fb7e7aa1463d3289a351287471f00116
StevenZhang3125 <2042427@learn.vsb.bc.ca>
2025-11-12 21:33:32 +0000

    example

--- 5.1/image_open.py
+++ 5.1/image_open.py
@@ -1,4 +1,12 @@
 from PIL import Image
 
-image_green = Image.open("CS11/5.1/kid-green.jpg").load()
-image_beach = Image.open("CS11/5.1/beach.jpg").load()
+image_green = Image.open("5.1/kid-green.jpg").load()
+image_beach = Image.open("5.1/beach.jpg").load()
+
+def is_green(r,g,b):
+    if r >= 0 and r < 25 and g > 230 and g <= 255 and  b>= 0 and b < 25:
+        return True
+    return False
+
+print(is_green(image_green[0, 0][0],image_green[0, 0][1],image_green[0, 0][2]))
+        
