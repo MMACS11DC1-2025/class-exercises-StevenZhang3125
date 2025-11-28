a78d2ad70413aa05757b3c7e8e068e4b1fde49c4
StevenZhang3125 <2042427@learn.vsb.bc.ca>
2025-11-28 21:13:29 +0000

    Utils

--- 6.7/utils.py
+++ 6.7/utils.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import math
 
 def colourInterest(file):
     grayScale = [] # Gray (Colour consistancy; Full white/black could affect data)
@@ -24,7 +25,7 @@ def colourInterest(file):
     return avgGray
 
 def otherColours(file, avgGray):
-    blacklist = []
+    blacklistRaw = []
     w = file.width
     h = file.height
     pixels = file.load()
@@ -37,11 +38,21 @@ def otherColours(file, avgGray):
     for col in range(0, w, stepW):
         for row in range(0, h, stepH):
             r, g, b = pixels[col,row]
-            avg = (r+g+b)//3
-            avgRange = (avg-2, avg+2)
-            if not(avgRange[0] < r and r < avgRange[1] and avgRange[0] < g and g < avgRange[1] and avgRange[0] < b and b < avgRange[1]):
+            avgRange = (avgGray-2, avgGray+2)
+            isGray = avgRange[0] < r and r < avgRange[1] and avgRange[0] < g and g < avgRange[1] and avgRange[0] < b and b < avgRange[1]
+            if not isGray:
+                blacklistRaw.append(bfs(file, (col, row), (r, g, b)))
 
-def bfs(file, start, colours, visited):
+def deadZone(file, blacklistRaw):
+    blacklist = []
+    for i in range(len(blacklistRaw)):
+        minX, maxX = nestedMinMaxFinder(blacklistRaw[i])
+        minY, maxY = nestedMinMaxFinder(blacklistRaw[i])
+        blacklist.append((min, max))
+    
+        
+def bfs(file, start, colours):
+    visited = []
     queue = [start]
     w = file.width
     h = file.height
@@ -52,4 +63,32 @@ def bfs(file, start, colours, visited):
             continue
         visited.add((col, row))
         r, g, b = pixels[col, row]
-        colourRange = colours[(r, g, b)]
+        colourRange = [(colours[0]-10, colours[0]+10), (colours[1]-10, colours[1]+10), (colours[2]-10, colours[2]+10)]
+        inRange = colourRange[0][0] < r and r < colourRange[0][1] and colourRange[1][0] < g and g < colourRange[1][1] and colourRange[2][0] < b and b < colourRange[2][1]
+        # Left
+        if col != 0:
+            if inRange:
+                queue.append((col-1, row))
+        # Right
+        if col != w-1:
+            if inRange:
+                queue.append((col+1, row))
+        # Up
+        if row != 0:
+            if inRange:
+                queue.append((col, row-1))
+        # Down
+        if row != h-1:
+            if inRange:
+                queue.append((col, row+1))
+    return visited
+
+def nestedMinMaxFinder(array):
+    minVal = math.inf
+    maxVal = -math.inf
+    for i in range(len(array)):
+        if array[i] < minVal:
+            minVal = array[i]
+        if array[i] > maxVal:
+            maxVal = array[i]
+    return minVal, maxVal
