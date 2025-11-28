c088283d87ac4f4cd7ea7d370e16be161af29008
StevenZhang3125 <2042427@learn.vsb.bc.ca>
2025-11-28 20:15:08 +0000

    Utils

--- 6.7/utils.py
+++ 6.7/utils.py
@@ -34,11 +34,22 @@ def otherColours(file, avgGray):
         stepW = 1
     if stepH == 0:
         stepH = 1
-    # for col in range(0, w, stepW):
-    #     for row in range(0, h, stepH):
-    #         r, g, b = pixels[col,row]
-    #         avg = (r+g+b)//3
-    #         if avg > 200 or avg < 50:
-    #             continue
-    #         avgRange = (avg-2, avg+2)
-    #         if avgRange[0] < r and r < avgRange[1] and avgRange[0] < g and g < avgRange[1] and avgRange[0] < b and b < avgRange[1]:
+    for col in range(0, w, stepW):
+        for row in range(0, h, stepH):
+            r, g, b = pixels[col,row]
+            avg = (r+g+b)//3
+            avgRange = (avg-2, avg+2)
+            if not(avgRange[0] < r and r < avgRange[1] and avgRange[0] < g and g < avgRange[1] and avgRange[0] < b and b < avgRange[1]):
+
+def bfs(file, start, colours, visited):
+    queue = [start]
+    w = file.width
+    h = file.height
+    pixels = file.load()
+    while queue:
+        col, row = queue.pop(0)
+        if (col, row) in visited:
+            continue
+        visited.add((col, row))
+        r, g, b = pixels[col, row]
+        colourRange = colours[(r, g, b)]
